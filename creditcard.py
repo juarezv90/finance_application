746e68606eb52ae3e9afb3593cf4fbd832d16bde
Victor Juarez <juarezv1990@gmail.com>
2025-05-28 16:33:39 -0400

    Intial push of Financial Underdog

---
+++ creditcard.py
@@ -0,0 +1,18 @@
+class Debt():
+    def calculate_payments(self, balance, apr):
+        pass
+    def payment_made(self):
+        pass
+    def add_balance(self):
+        pass
+
+
+class CreditCard(Debt):
+    card_name = ""
+    balance = 0
+    APR = 0
+
+    def __init__(self, card_name, balance, apr):
+        self.card_name = card_name
+        self.balance = float(balance)
+        self.APR = apr
